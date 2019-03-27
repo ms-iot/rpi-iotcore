725c05747b75142900fe032402fb289ab88698c0
Henry Nie <henie@microsoft.com>
2019-03-27 18:00:43 +0800

    Set Network MAC address in mailbox if it is not set for Lan7800. Restart Lan7800 device on first boot if MAC address is set.

---
+++ drivers/RpiLanPropertyChange/bcm2836/dllmain.cpp
@@ -0,0 +1,44 @@
+//
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//
+// dllmain.cpp : Defines the entry point for the DLL application.
+
+#include "stdafx.h"
+
+HINSTANCE g_hInstance = nullptr;
+
+extern "C"
+{
+    // Externally callable functions
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    {
+        // Save the dll module handle
+        g_hInstance = hModule;
+
+        // Disable thread-attach calls
+        DisableThreadLibraryCalls(hModule);
+    }
+    break;
+
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    break;
+
+    case DLL_PROCESS_DETACH:
+    {
+        g_hInstance = NULL;
+    }
+    break;
+    }
+    return TRUE;
+}
+
+} // extern "C"
+
